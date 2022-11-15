c9b816bd671f82f838260dfe7485af4d41b5eeec
dawmro <dawidmrotek@protonmail.ch>
2022-11-15 00:32:53 -0800

    add register user

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
@@ -11,4 +13,27 @@ def index(request):
 def counter(request):
     words = request.POST['words']
     amount_of_words = len(words.split())
-    return render(request, 'counter.html', {'amount': amount_of_words})
+    
+
+def register(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "Username taken by other user")
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, "Email taken by other user")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save();
+                return redirect('login')
+            
+
+    return render(request, 'register.html')
